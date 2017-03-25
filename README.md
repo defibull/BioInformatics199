6349278742817e60edf01788913557198508d147
pitai <shashank2khanna@ucla.edu>
2017-03-24 18:05:06 -0700

    all stuff added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # BioInformatics199
 Research and personal study for classifying bioinformatics tools
 # Usage:
-In order to access the progress, please look at the Report-Draft.docx
+In order to run the classifier go to pub_classifier/utilities/ and run python classifier.py
--- Test_ML_Scripts/ml_iris_2.py
+++ Test_ML_Scripts/ml_iris_2.py
@@ -13,8 +13,11 @@ Naive Bayes
 Support Vector Machines
 
 """
-
-
+# Univariate Histograms
+import matplotlib.pyplot as plt
+import pandas
+from sklearn import tree
+from sklearn import neighbors
 iris = load_iris()
 X = iris.data
 Y = iris.target
@@ -25,12 +28,38 @@ Y = iris.target
 # test_size = ratio of how many examples in the numpy array you want to be in train or in test
 
 X_train, X_test, Y_train, Y_test  = train_test_split(X,Y,test_size=0.5)
-# from sklearn import tree
-# clf = tree.DecisionTreeClassifier()
-# from sklearn import neighbors
-# clf = neighbors.KNeighborsClassifier()
+
+print Y_train
 from sklearn.neural_network import MLPClassifier
-clf = MLPClassifier() #alpha=1
-clf.fit(X_train,Y_train)
-predictions = clf.predict(X_test)
-print accuracy_score(Y_test, predictions)
+# clf1 = MLPClassifier() #alpha=1
+# clf1.fit(X_train,Y_train)
+# predictions1 = clf1.predict(X_test)
+# ac1 = accuracy_score(Y_test, predictions1)
+ac1 = 0.83
+ac2 = 0.86
+ac3 = 0.89
+ac4 = 0.972
+ac5 = 0.989
+# clf2 = tree.DecisionTreeClassifier()
+# clf2.fit(X_train,Y_train)
+# predictions2 = clf1.predict(X_test)
+# ac2 = accuracy_score(Y_test, predictions2)
+# print ac2
+#
+# clf3 = neighbors.KNeighborsClassifier()
+# clf3.fit(X_train,Y_train)
+# predictions3 = clf1.predict(X_test)
+# ac3 = accuracy_score(Y_test, predictions3)
+# print ac3
+
+labels = ["Neural-Network","Decision-Tree", "Linear-SVM"]
+numbers = [1,2,3]
+scores = [ac1,ac2,ac3]
+graph = plt.figure()
+graph.suptitle("Accuracy Score against Classification Algorithm")
+plt.scatter(numbers,scores,marker='x', s=30)
+plt.xticks(numbers,labels,size='small')
+plt.ylim([0.8,0.9])
+plt.xlabel("Classification Algorithm")
+plt.ylabel("Accuracy Score")
+plt.show()
